4a7a1232963836f78604d6667ae8773238f3b229
Christopher <casheghian@gmail.com>
2013-03-13 16:09:39 +0100

    Stlying

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,5 @@
 class Search < ActiveRecord::Base
-  attr_accessible :cuisine_input, :keywords, :offer_input
+  attr_accessible :cuisine_input, :keywords, :offer_input,
 
 def restaurants
   @restaurants ||= find_restaurants
@@ -9,8 +9,9 @@ private
 	def find_restaurants
 	  restaurants = Restaurant.order(:id) 
 	  restaurants = restaurants.where("name like ?", "%#{keywords}%") unless keywords.blank?
-	  restaurants = restaurants.tagged_with("#{cuisine_input}") unless cuisine_input.blank?
-	  restaurants = restaurants.tagged_with("#{offer_input}")  unless offer_input.blank?
+	  restaurants = restaurants.tagged_with("#{cuisine_input.join(",")}",:any => true) unless cuisine_input.blank?
+	  restaurants = restaurants.tagged_with("#{offer_input.join(",")}",:any => true)  unless offer_input.blank?
+
 	  restaurants
 	end
 end
