17c695f73c9cdbe7aa3bb137e939a95a13b1a2d9
Christopher <casheghian@gmail.com>
2013-05-02 19:08:57 +0200

    max_party_input test

--- app/models/search.rb
+++ app/models/search.rb
@@ -21,7 +21,7 @@ private
 	  restaurants = restaurants.where("cuisines.name in (?)", cuisine_input) unless cuisine_input.blank?
 	  restaurants = restaurants.where("offer in (?)", offer_input) unless offer_input.blank?
 	  restaurants = restaurants.where("availability in (?)", availability_input) unless availability_input.blank?
-	  restaurants = restaurants.where("max_party >= (?)", max_party_input) unless max_party_input.blank?
+	  restaurants = restaurants.where("max_party >= ?", max_party_input) unless max_party_input.blank?
 
 
 	  #restaurants = restaurants.tagged_with(offer_input,:on => :offer, :any => true)  unless offer_input.blank?
